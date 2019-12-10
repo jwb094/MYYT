b1321e7c7f7471e382113cbd591df4e8b3a3aafe
Josh Bello <joshbello@joshs-mbp.home>
2019-12-10 19:30:50 +0000

    npm install multiple youtube package to see different logic and accessing data from service

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import YTSearch from 'youtube-api-search';
+import searchYoutube from 'youtube-api-v3-search';
+import YoutubeSearch from "youtube-api-search-reloaded";
+import betterYT from 'better-youtube-api';
 import Searchbar from './components/searchbar';
 import api from './components/apicall';
 
@@ -14,15 +17,72 @@ class App extends React.Component {
         videos:[],
         sterm : ''
     };
-    
-    Value = (term) => {
+    //YTSearch
+   Value = (term) => {
       console.log(term);
-      YTSearch({key:KEY,maxResults: 100,term}, videos => {
+      YTSearch({key:KEY,term}, videos => {
         console.log(videos);
       });
-      
     }
+//searchYoutube
+
+  /*  Value = (term) => {
+
+     const options = {
+      q:term,
+      part:'snippet',
+      type:'video',
+    }
+   
+    let result =  searchYoutube(KEY,options)
+    .then(response => { 
+      console.log(response)
+    })
+    .catch(error => {
+        console.log(error.response)
+    });
+    console.log(result);
+    }*/
+
+    //YoutubeSearch
+   /* Value = (term) => {
+    const params = {
+      part:'snippet',
+      key:KEY,
+      term:term,
+      type:'video',
     
+
+    };
+     
+
+    // .then(response => { 
+    //   console.log(response)
+    // })
+    // .catch(error => {
+    //     console.log(error.response)
+    // });
+    YoutubeSearch({ params })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }*/
+  //betterYT 
+  /*Value = (term) => {
+    const youtube = new betterYT(KEY);
+    const videos = youtube.searchVideos(term, 20)
+      .then(response => { 
+      console.log(response)
+    })
+    .catch(error => {
+        console.log(error.response)
+    });
+    */
+// console.log(videos) // array of 12 partial video objects
+  //}
       // const response = await api.get('/search', {
       //     params: {
       //       part: 'snippet',
@@ -38,7 +98,7 @@ class App extends React.Component {
   
 
      
-    //}
+   // }
   render(){
   return (
     <Searchbar handleFormSubmit={this.Value}/>

f4d75159cda03d411412149e96cd9b034a638b75
Josh Bello <joshbello@joshs-mbp.home>
2020-01-06 19:59:40 +0000

    videolist: changed html element

--- src/components/videolist.js
+++ src/components/videolist.js
@@ -1,11 +1,5 @@
 import React from 'react';
-
-const Videolist = ({videos, selected}) => {
-    const rendervideolist = videos.map((video) => { //foreach of new array of videos list
-     //  console.log(video);
-        return(
-            
-                <li className="list-group-item" onClick={() => selected(video)}>
+                {/* <li className="list-group-item" onClick={() => selected(video)}>
                     <div className="col-sm-12">
                         <img src= {video.snippet.thumbnails.default.url}></img>
                         
@@ -13,8 +7,26 @@ const Videolist = ({videos, selected}) => {
                     <div className="col-sm-12">
                         {video.snippet.title}
                     </div>
-                </li>
-           
+                
+             
+             
+                </li> */}
+const Videolist = ({videos, selected}) => {
+    const rendervideolist = videos.map((video) => { //foreach of new array of videos list
+     //  console.log(video);
+        return(
+            
+
+ 
+              <div class="row" onClick={() => selected(video)}>
+                <div class="col">
+                <img src= {video.snippet.thumbnails.default.url}></img>
+                </div>
+                <div class="col">
+                {video.snippet.title}
+                </div>
+                <br/>   
+            </div>
 )
     })
     return rendervideolist;
